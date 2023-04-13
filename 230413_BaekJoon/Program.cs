a7351809a9f5e2bf21b6a4fc96630b12a9262a61
simgksk <Daum_c0m@naver.com>
2023-04-13 12:44:08 +0900

    초기 커밋.

---
+++ 230413_BaekJoon/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _230413_BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+        }    
+    }
+}
